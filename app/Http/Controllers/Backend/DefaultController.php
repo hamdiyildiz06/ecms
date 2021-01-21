da55e94b0663ec0a7b71ef7be35faadd95a17099
hamdi Yıldız <hamdiyildiz06@gmail.com>
2021-01-21 17:58:26 +0300

    Yönetici giriş işlemleri login auth kontrolü yapıldı 33

--- app/Http/Controllers/Backend/DefaultController.php
+++ app/Http/Controllers/Backend/DefaultController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DefaultController extends Controller
 {
@@ -13,5 +14,18 @@ class DefaultController extends Controller
 
     public function login(){
         return view('backend.default.login');
+    }
+
+    public function authenticate(Request $request){
+        $request->flash();
+        $credentials = $request->only('email','password');
+
+        $remember_me = $request->has('remember_me') ? true : false;
+
+        if (Auth::attempt($credentials, $remember_me)){
+            return redirect()->intended(route('nedmin.Index'));
+        }else{
+            return back()->with('error','Hatalı Kullanıcı');
+        }
     }
 }
--- resources/views/backend/default/login.blade.php
+++ resources/views/backend/default/login.blade.php
@@ -48,20 +48,25 @@
     <div class="login-box-body">
         <p class="login-box-msg">Kullanıcı Adı ve Şifrenizi Giriniz</p>
 
-        <form action="../../index2.html" method="post">
+        @if(Session::has('error'))
+            <div class="alert alert-danger">{{session('error')}}</div>
+        @endif
+
+        <form action="{{route('nedmin.Authenticate')}}" method="post">
+            @CSRF
             <div class="form-group has-feedback">
-                <input type="email" class="form-control" name="email" placeholder="Kullanıcı E-mail">
+                <input type="email" required class="form-control" name="email" value="{{old('email')}}" placeholder="Kullanıcı E-mail">
                 <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
             </div>
             <div class="form-group has-feedback">
-                <input type="password" class="form-control" name="password" placeholder="Şifreniz">
+                <input type="password" required class="form-control" name="password" placeholder="Şifreniz">
                 <span class="glyphicon glyphicon-lock form-control-feedback"></span>
             </div>
             <div class="row">
                 <div class="col-xs-8">
                     <div class="checkbox icheck">
                         <label>
-                            <input type="checkbox"> Beni Hatırla
+                            <input type="checkbox" name="remember_me" {{old('remember_me') ? 'checked' : null}}> Beni Hatırla
                         </label>
                     </div>
                 </div>
